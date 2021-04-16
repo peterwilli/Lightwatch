61aaadeadc599e9d9942522551a00489529f0e8e
Peter Willemsen <peter@codebuffet.co>
2021-04-16 16:30:03 +0200

    libc low level allocation for the esp32 in rust + added a extra thing to the patch

--- rust_firmware/src/lib.rs
+++ rust_firmware/src/lib.rs
@@ -1,11 +1,20 @@
 #![no_std]
+#![feature(default_alloc_error_handler)]
 use panic_halt as _;
+mod libc_alloc;
+use libc_alloc::*;
 mod c_bindings;
 use c_bindings::*;
+extern crate alloc;
+use alloc::boxed::Box;
+
+#[global_allocator]
+static A: LibcAllocator = LibcAllocator;
 
 #[no_mangle]
 pub extern "C" fn rust_bb_init() {
     unsafe {
         fillScreen(10);
     }
+    let _: Box<[u8]> = Box::new([0; 10]);
 }
---
+++ rust_firmware/src/libc_alloc.rs
@@ -0,0 +1,19 @@
+use core::alloc::{GlobalAlloc, Layout};
+extern {
+    fn malloc(size: usize) -> *mut u8;
+    fn free(ptr: *mut u8);
+    fn write(file: isize, buffer: *const u8, count: usize) -> usize;
+}
+
+pub struct LibcAllocator;
+unsafe impl GlobalAlloc for LibcAllocator {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        if layout.align() > 8 {
+            panic!("Unsupported alignment")
+        }
+        malloc(layout.size())
+    }
+    unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
+        free(ptr)
+    }
+}
